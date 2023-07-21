b31e508179dd47772312a28167778131ab673e7f
Deyvered <112627655+Deyvered@users.noreply.github.com>
2023-07-21 17:30:02 -0600

    voy avanzando

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -50,6 +50,15 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   if (idioma === "aleman") {
+      return "Guten Tag";
+   } else if (idioma === "mandarin") {
+      return "Ni Hao";
+   } else if (idioma === "ingles") {
+      return "Hello!";
+   } else {
+      return "Hola!";
+   }
 }
 
 function colors(color) {
@@ -61,12 +70,29 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+   switch (color) {
+      case "blue":
+         return "This is blue";
+      case "red":
+         return "This is red";
+      case "green":
+         return "This is green";
+      case "orange":
+         return "This is orange";
+      default:
+         return "Color not found";            
+   }
 }
 
 function esDiezOCinco(num) {
    // Retornar true si "num" es 10 o 5.
    // De lo contrario, retornar false.
    // Tu código:
+   if (num === 10 || num === 5) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function estaEnRango(num) {
